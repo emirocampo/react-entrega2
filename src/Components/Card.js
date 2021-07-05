1006dd09f7fc4bf281a14cde8dd3f32102dde06c
emiro campo <emiroluiscampo@gmail.com>
2021-07-05 12:58:20 -0500

    version: 1.0.2 se agregó color de fondo. no sirve la busqueda por ciudad

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,25 +2,34 @@ import React from 'react'
 
 
 export default function Card({
-                                city, 
+                                city,
+                                country, 
                                 temperature, 
                                 temperatureFeelsLike, 
                                 humidity,
-                                weather,
+                                weatherMain,
+                                weatherDescription,
                                 wind,
                                 icon,
                                 conversion,
-                                temperatureUnit
+                                temperatureUnit,
                             }) {
     return (
         <>
           <div className="col-12 col-sm-10 col-md-8 col-lg-5 col-xl-5 col-xxl-5 align-items-center">
             <div className="card ">
                 <div className="card-header">
-                    App del clima
+                    App del clima  
                 </div>
                 <div className="card-body">
-                    <h5 className="card-title">Ciudad: {city}</h5>
+                    <input type="input" className="form-control"  placeholder="ciudad"
+                            
+                    />
+                    <button type="button" className="btn btn-secondary mt-2" 
+                            >Buscar</button>
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">Ciudad: {city} - {country}</h5>
                     <div className="card-text">
                         <div className="row justify-content-center align-items-center">
                             <div className="col text-center">
@@ -30,7 +39,7 @@ export default function Card({
                                 <div>Temperatura: {temperature}{temperatureUnit}</div> 
                                 <div>Sensación Termica: {temperatureFeelsLike}{temperatureUnit}</div>
                                 <div>Humedad: {humidity} %</div>
-                                <div>{weather}</div>
+                                <div>{weatherMain}: {weatherDescription}</div>
                                 <div>Viento: {wind} m/s</div>
                             </div>                           
                         </div>
--- src/Components/Container.js
+++ src/Components/Container.js
@@ -8,22 +8,28 @@ export default function Container() {
     const [coordLatitude, setCoordLatitude] = useState("");
     const [coordLongitude, setCoordLongitude] = useState("");
     const [city, setCity] = useState("");
+    const [country, setCountry] = useState("");
     const [temperature, setTemperature] = useState("");
     const [temperatureFeelsLike, setTemperatureFeelsLike] = useState("");
     const [humidity, setHumidity] = useState("");
-    const [weather, setWeather] = useState("");
+    const [weatherMain, setWeatherMain] = useState("");
+    const [weatherDescription, setWeatherDescription] = useState("");
     const [wind, setWind] = useState("");
     const [icon, setIcon] = useState("")
     const [isCelsius, setIsCelsius] = useState(true)
     const [temperatureUnit, setTemperatureUnit] = useState(" °C")
     const [statusCode, setStatutsCode] = useState("")
+    const [color, setColor] = useState("#000000");
+    
 
     const setSiteData = (data) =>{        
-        setCity(data.name+" - "+data.sys.country);        
+        setCity(data.name);        
+        setCountry(data.sys.country);        
         setTemperature(data.main.temp);        
         setTemperatureFeelsLike(data.main.feels_like);        
-        setHumidity(data.main.humidity);        
-        setWeather(data.weather[0].main+" : "+data.weather[0].description);        
+        setHumidity(data.main.humidity);
+        setWeatherMain(data.weather[0].main);        
+        setWeatherDescription(data.weather[0].description);        
         setWind(data.wind.speed);
         setIcon(`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
         setStatutsCode(data.cod)
@@ -55,7 +61,7 @@ export default function Container() {
             fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coordLatitude}&lon=${coordLongitude}&appid=7ddfea8617fa720d4d15916bb1503482&units=metric&lang=es`)
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data)
+                    // console.log(data)
                     setSiteData(data)
                 });
         }
@@ -89,10 +95,41 @@ export default function Container() {
 
     useEffect(() => {
         setStatutsCode( prevState => { 
-            console.log(prevState)
             return prevState })
         
     }, [statusCode])
+
+    useEffect(() => {
+        switch (weatherMain) {
+            case "Thunderstorm":
+                setColor("#884ea0");
+                break;
+            case "Drizzle":
+                setColor("#d4e6f1");
+                break;
+            case "Rain":
+                setColor("#5499c7");
+                break;
+            case "Snow":
+                setColor("#f2f4f4");
+                break;
+            case "Atmosphere":
+                setColor("#808b96");
+                break;
+            case "Clear":
+                setColor("#3498db");
+                break;
+            case "Clouds":
+                setColor("#909497");
+                break;
+        
+            default:
+                setColor("#ffffff");
+                break;
+        }
+        
+        
+    }, [weatherMain])
 
     const toCelsiusFarenheit = () =>{
         setIsCelsius( (isCelsius ? false : true) )
@@ -101,19 +138,22 @@ export default function Container() {
     if (statusCode === 200) {
         return (
             <>
-              <div className="container">
+              <div className="container" style={{backgroundColor:color}}>
                 <div className="row vh-100 justify-content-center align-items-center">
-                    <h1>OJO CON EL CONSOLE.LOG DEL FETCH!!!</h1>                
+                    {/* <h1>OJO CON EL CONSOLE.LOG DEL FETCH!!!</h1>                 */}
                     <Card 
-                        city={city} 
+                        city={city}
+                        country={country} 
                         temperature={temperature}
                         temperatureFeelsLike={temperatureFeelsLike}
                         humidity={humidity}
-                        weather={weather}
+                        weatherMain={weatherMain}
+                        weatherDescription={weatherDescription}
                         wind={wind}
                         icon={icon}
-                        conversion = {toCelsiusFarenheit}
+                        conversion={toCelsiusFarenheit}
                         temperatureUnit={temperatureUnit}
+                        
                     />
                 </div>
               </div>  
